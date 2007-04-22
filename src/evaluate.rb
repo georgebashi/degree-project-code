c615bbb2131c768724f0056bdbc7ebef1acf9855
George Bashi <jamil@georgebashi.com>
2007-04-22 00:06:16 +0000

    * Evaluator now gives more stats

--- src/evaluate.rb
+++ src/evaluate.rb
@@ -6,16 +6,46 @@ require 'find'
 require 'rubygems'
 require 'inline'
 
-@results = Array.new
+class Fixnum
+	N_BYTES = [42].pack('i').size
+	N_BITS = N_BYTES * 8
+	MAX = 2 ** (N_BITS - 2) - 1
+	MIN = -MAX - 1
+end
 
-@song_count = (`./generator -v --dir=../test --similar=1 ../test/Hot\ Chip/Coming\ on\ Strong/Coming\ on\ Strong-11-One\ One\ One.ogg.vec`.split)[0].to_i
-puts "Generator has #{@song_count} songs."
-
-File.open("results.txt").each { |line|
-	entry = Array.new
-	entry += line.split(" * ")
-	@results << entry
-}
+class Array
+	def average
+		total = 0
+		self.each { |num|
+			total += num
+		}
+		Float(total) / self.size
+	end
+	
+	def stdev(avg)
+		total = 0
+		self.each { |num|
+			total += (num - avg) ** 2
+		}
+		Math.sqrt((1.0 / self.size) * total)
+	end
+	
+	def max
+		cur = 0
+		self.each { |num|
+			cur = num if num > cur
+		}
+		cur
+	end
+	
+	def min
+		cur = Fixnum::MAX
+		self.each { |num|
+			cur = num if num < cur
+		}
+		cur
+	end
+end
 
 def get_similar(track, comparison)
 	playlist = Array.new
@@ -47,27 +77,35 @@ def test(comparison)
 	scores
 end
 
-class Array
-	def average
-		total = 0
-		self.each { |num|
-			total += num
-		}
-		Float(total) / self.size
-	end
-	
-	def stdev(avg)
-		total = 0
-		self.each { |num|
-			total += (num - avg) ** 2
-		}
-		Math.sqrt((1.0 / self.size) * total)
-	end
-end
+@results = Array.new
+
+File.open("results.txt").each { |line|
+	entry = Array.new
+	entry += line.split(" * ")
+	@results << entry
+}
+
+@song_count = (`./generator -v --dir=../test --similar=1 ../test/Hot\ Chip/Coming\ on\ Strong/Coming\ on\ Strong-11-One\ One\ One.ogg.vec`.split)[0].to_i
+puts "Generator has #{@song_count} songs."
 
 for comparison in ["ordered", "ordered-area", "sorted", "total"]
-	scores = test(comparison)
-	score_avg = scores.average
-	score_stdev = scores.stdev(score_avg)
-	puts "#{comparison} has avg #{score_avg} and stdev #{score_stdev}"
+	pos_similar = Array.new
+	pos_not_similar = Array.new
+	@results.each { |result|
+		playlist = get_similar(result[0], comparison)
+		pos_similar << playlist_position(playlist, result[1])
+		pos_not_similar << playlist_position(playlist, result[2])
+	}
+
+	similar_avg = pos_similar.average
+	similar_stdev = pos_similar.stdev(similar_avg)
+	similar_high = pos_similar.max
+	similar_low = pos_similar.min
+    not_similar_avg = pos_not_similar.average
+    not_similar_stdev = pos_not_similar.stdev(not_similar_avg)
+    not_similar_high = pos_not_similar.max
+    not_similar_low = pos_not_similar.min
+
+	score = similar_avg + (@song_count - not_similar_avg)
+	puts "#{comparison} has similar_avg #{similar_avg} stdev #{similar_stdev} high #{similar_high} low #{similar_low}, not_similar_avg #{not_similar_avg} stdev #{not_similar_stdev} high #{not_similar_high} low #{not_similar_low}, score #{score}"
 end
